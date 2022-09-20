ed9e00b59bc8c2142c40c9c1151e8a26d42125bb
vojinpavlovic <vojinpavlovic@outlook.com>
2022-09-20 13:44:31 +0200

    [REMOVE] src/config/Session -> src/Application/Configuration/Session

--- src/Application/Session/AddSession.ts
+++ src/Application/Session/AddSession.ts
@@ -4,4 +4,5 @@ import session from "express-session";
 import RedisService from "../../Infrastructure/Services/RedisService";
 import sessionConfig from '../Configuration/Session';
 
-export default (app: Application) => app.use(session(sessionConfig(RedisService.getStore(session)))); 
+export default (app: Application) => 
+    app.use(session(sessionConfig(RedisService.getStore(session)))); 
