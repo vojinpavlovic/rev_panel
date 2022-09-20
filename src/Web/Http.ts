f5a7154b245eed5c82e5d40eb521485207f8e03f
vojinpavlovic <vojinpavlovic@outlook.com>
2022-09-20 10:39:41 +0200

    [ADD] Http server

---
+++ src/Web/Http.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import session from "express-session";
+import cors from 'cors';
+import router from '../Web/Router'
+import { sessionConfig } from "../config/Session";
+
+import { SteamOAuth } from "../Infrastructure/Services/SteamOAuth";
+const steamOAuth = new SteamOAuth().setup();
+
+export class Http {
+    private app: express.Application = express();
+    private port: number = parseInt(<string>process.env.PORT ?? 3000);
+
+    public registerMiddlewares(): void {
+        this.app.use(cors())
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(session(sessionConfig));
+        this.app.use(steamOAuth.initialize());
+        this.app.use(steamOAuth.session());
+    }
+
+    public setup(): void {
+        this.registerMiddlewares();
+        router(this.app)
+        this.app.listen(this.port, () => 
+            console.log([`🚀 Listening on http://localhost:${this.port}`])
+        );
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -2,27 +2,7 @@ import "reflect-metadata";
 import { config } from "dotenv";
 config();
 
-import express from "express";
-import session from "express-session";
-import cors from 'cors';
+import { Http } from "./Web/http";
+new Http().setup();
 
-import { sessionConfig } from "./config/Session";
-import { SteamOAuth } from "./Infrastructure/Services/SteamOAuth";
 
-import router from './Web/Router'
-
-const app = express();
-const port = parseInt(<string>process.env.PORT ?? 3000);
-
-app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(session(sessionConfig));
-
-const steamOAuth = new SteamOAuth().setup();
-app.use(steamOAuth.initialize());
-app.use(steamOAuth.session());
-
-router(app);
-
-app.listen(port, () => console.log([`🚀 Listening on http://localhost:${port}`]));
