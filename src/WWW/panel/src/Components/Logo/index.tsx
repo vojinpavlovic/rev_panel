b6741021c4f9383804b641edad745f641fdb0fda
Vojin <vojin@Vojins-Mac-mini.local>
2022-09-29 20:39:38 +0200

    [ADD] Logo bounce effect

--- src/WWW/panel/src/Components/Logo/index.tsx
+++ src/WWW/panel/src/Components/Logo/index.tsx
@@ -3,7 +3,7 @@ import logoImg from '../../Static/Images/logo-horizontal.png';
 import { motion } from 'framer-motion';
 
 const logo = () => <motion.img 
-    animate={{y: [-200, 10, 0]}}
+    animate={{y: [-200, 10, -10, 0, -5, 0]}}
     transition={{delay: 0.55}}
     src={logoImg} 
     alt="logo"
