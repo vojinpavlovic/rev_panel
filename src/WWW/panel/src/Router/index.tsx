97e51ea0433c15267c802f347b1f70e615242fbb
vojinpavlovic <vojinpavlovic@outlook.com>
2022-10-05 13:57:14 +0200

    [ADD] Loading and UserQuery fixed (Backend)

--- src/WWW/panel/src/Router/index.tsx
+++ src/WWW/panel/src/Router/index.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Loading from '../Components/Loading';
 import useUser from '../Hooks/useUser';
 import Dashboard from '../Pages/Dashboard';
 import { UserState } from '../Types/UserState';
@@ -7,14 +8,15 @@ import ProtectedRoute from './ProtectedRoute';
 const Router = () => {
     const { user, loading }: UserState = useUser()
 
-    if (loading) return <h1>Ucitavanje...</h1>
+    if (loading) return <Loading/>
 
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<ProtectedRoute user={user}/>}>
                     <Route path="/dashboard" element={<Dashboard/>}/>
-                </Route>                
+                </Route> 
+                <Route path="/loading" element={<Loading/>}/>              
             </Routes>
         </BrowserRouter>
     );
