573170562bca7b170985f1460999e3612f17b840
vojinpavlovic <vojinpavlovic@outlook.com>
2022-10-10 11:39:04 +0200

    [ADD] Style to Auth page

--- src/WWW/panel/src/Components/SimpleLayout/index.tsx
+++ src/WWW/panel/src/Components/SimpleLayout/index.tsx
@@ -1,5 +1,7 @@
 import { motion } from 'framer-motion'
 import ChildrenProp from '../../Types/ChildrenProp'
+import SmokeMotion from '../SmokeMotion'
+import BackArrow from '../SVG/BackArrow'
 
 const style = `
     flex
@@ -9,14 +11,21 @@ const style = `
     text-white
     h-screen
     w-screen
+    relative
 `
 
 const SimpleLayout = ({children}: ChildrenProp): JSX.Element => {
     return (
         <motion.div animate={{opacity: [0, 1]}} className={style}>
+            <motion.button className='absolute left-5 top-5 flex justify-center items-center gap-5 opacity-70 hover:opacity-100 duration-300'>
+                <BackArrow/> Pocetna
+            </motion.button>
+            <SmokeMotion/>
             {children}
+            <p className='absolute bottom-2 text-xs'>Sva prava zadrzana @ Revolucija Roleplay</p>
         </motion.div>
     )
 }
+
 
 export default SimpleLayout
