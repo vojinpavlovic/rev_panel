0faa41c8b00a70e2906dbea8d62b39927430b313
Vojin Pavlovic <vojin.pavlovic@vega.local>
2022-11-28 13:28:34 +0100

    [FIX] Navigation between frontends

--- frontend/panel/src/Components/SimpleLayout/index.tsx
+++ frontend/panel/src/Components/SimpleLayout/index.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion'
+import { useNavigate } from 'react-router-dom'
 import ChildrenProp from '../../Types/ChildrenProp'
 import SmokeMotion from '../SmokeMotion'
 import BackArrow from '../SVG/BackArrow'
@@ -17,9 +18,9 @@ const style = `
 const SimpleLayout = ({children}: ChildrenProp): JSX.Element => {
     return (
         <motion.div animate={{opacity: [0, 1]}} className={style}>
-            <motion.button className='absolute left-5 top-5 flex justify-center items-center gap-3 text-xl opacity-70 hover:opacity-100 duration-300'>
+            <motion.a href="https://revolucija-roleplay.com" className='absolute left-5 top-5 flex justify-center items-center gap-3 text-xl opacity-70 hover:opacity-100 duration-300'>
                 <BackArrow/> Pocetna
-            </motion.button>
+            </motion.a>
             <SmokeMotion/>
             {children}
             <p className='absolute bottom-2 text-xs'>Sva prava zadrzana @ Revolucija Roleplay</p>
