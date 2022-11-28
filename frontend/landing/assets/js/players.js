955a608021e5da990c97355315c133de2123ee8f
Vojin Pavlovic <vojin.pavlovic@vega.local>
2022-11-28 12:46:12 +0100

    [CRITICAL] Player count

--- frontend/landing/assets/js/players.js
+++ frontend/landing/assets/js/players.js
@@ -1,6 +1,6 @@
 const DYNAMIC_JSON = "https://api.revolucija-roleplay.com/api/server/dynamic"
 
-const getPlayers = async () =>  await fetch(DYNAMIC_JSON, {
+const getDynamic = async () =>  await fetch(DYNAMIC_JSON, {
         method: "GET",
         redirect: "follow",
         headers: {
@@ -14,8 +14,12 @@ const getPlayers = async () =>  await fetch(DYNAMIC_JSON, {
     })
 
 const updatePlayers = async () => {
-    const players = await getPlayers()
-    console.log(players)
+    const dynamic = await getDynamic()
+    const field = document.getElementById("player-count")
+
+    if (dynamic.status && dynamic.data) {
+        field.innerHTML = `${dynamic.data.clients} / ${dynamic.data.maxClients}`
+    }
 }
 
 updatePlayers()
