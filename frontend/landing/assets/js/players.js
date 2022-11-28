957dace7d019cf536308d596834bab0f8da1f76f
Vojin Pavlovic <vojin.pavlovic@vega.local>
2022-11-28 13:07:22 +0100

    [FIX] Fetch on landing page

--- frontend/landing/assets/js/players.js
+++ frontend/landing/assets/js/players.js
@@ -1,6 +1,20 @@
 const DYNAMIC_JSON = "https://api.revolucija-roleplay.com/api/server/dynamic"
+const PLAYERS_JSON = "https://api.revolucija-roleplay.com/api/server/players"
 
 const getDynamic = async () =>  await fetch(DYNAMIC_JSON, {
+        method: "GET",
+        redirect: "follow",
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .catch(err => {
+        return null
+    })
+
+const getPlayers = async () =>  await fetch(PLAYERS_JSON, {
         method: "GET",
         redirect: "follow",
         headers: {
@@ -19,6 +33,22 @@ const updatePlayers = async () => {
 
     if (dynamic.status && dynamic.data) {
         field.innerHTML = `${dynamic.data.clients} / ${dynamic.data.maxClients}`
+    }
+}
+
+const updatePlayersSide = async () => {
+    const players = await getPlayers()
+
+    if (players.status && players.data) {
+        for (const key of players.data) {
+            const {name, ping} = players.data[key]
+            $("#player-list").append(`
+                <div class="item">
+                    <p>${name}</p>
+                    <span>${ping}</span>
+                </div>
+            `)
+        }
     }
 }
 
